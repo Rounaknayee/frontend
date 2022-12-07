3d504e259a8ad90f6924504465e60a2814bfe40b
rounaknayee <rounaknayee007@gmail.com>
2022-12-06 19:00:37 -0500

    Tuesday

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { rooturl } from "../config";
+import Fileupload from "./Fileupload";
 
 export default function AdminDashboard() {
     return (
         <div>
         <h1>Admin Dashboard</h1>
+        <Fileupload />
+        
+
         </div>
     );
     }
